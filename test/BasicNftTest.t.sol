95736f96fc9e78a5a4c99258132345fb4b4dc20e
Shivannsh <bb1221392@iitd.ac.in>
2024-04-10 14:34:16 +0530

    BasicNft Deployed on Sepolia chain

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -11,7 +11,7 @@ contract BasicNftTest is Test {
     BasicNft public basicNft;
     DeployBasicNft public deployer;
     address public USER = address(1);
-    string  public constant PUB= "ipfs://bafybeicdlctvdhgvhnu5xqjm6tvjzaw3oyllq77deguvllb52hzu3ur76m/?filename=pug.png";
+    string  public constant PUB= "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     function setUp() public {
         deployer = new DeployBasicNft();
@@ -22,10 +22,10 @@ contract BasicNftTest is Test {
         assertEq(basicNft.name() ,"Dogie");
     }
 
-    function canMintAndHaveBalance() public {
-        vm.prank(USER);
-        basicNft.mintNft(PUB);
-        assert(basicNft.balanceOf(USER) == 1);
-        assert(keccak256(abi.encodePacked(basicNft.tokenURI(0))) == keccak256(abi.encodePacked(PUB)));
-    }
+    // function canMintAndHaveBalance() public {
+    //     vm.prank(USER);
+    //     basicNft.mintNft(PUB);
+    //     assert(basicNft.balanceOf(USER) == 1);
+    //     assert(keccak256(abi.encodePacked(basicNft.tokenURI(0))) == keccak256(abi.encodePacked(PUB)));
+    // }
 }
